6e3ec5dab5f183c9d617635e7c3344b75e4822b9
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-07-25 09:48:29 -0500

    feat: add working script

---
+++ src/index.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const { join } = require('path');
+const { writeFileSync } = require('fs');
+const { fileListFromPath } = require('filelist-utils');
+const { convertFileList, groupByExperiments } = require('brukerconverter');
+const { fromVariables } = require('convert-to-jcamp');
+const { xMultiply } = require('ml-spectra-processing');
+
+const pathToWrite = '/home/abolanos/';
+
+const converterOptions = {
+  converter: { xy: true },
+  filter: {
+    // experimentNumber: [120, 121, 122],
+    onlyFirstProcessedData: true,
+    ignoreFID: true,
+    ignore2D: true,
+  },
+};
+
+(async () => {
+  const fileList = fileListFromPath(join(__dirname, '../data/exam1d_1H'));
+  const experiments = groupByExperiments(fileList, converterOptions.filter);
+
+  for (const expno of experiments) {
+    const spectra = await convertFileList(expno.fileList);
+    writeFileSync(
+      join(pathToWrite, 'spectraBruker.json'),
+      JSON.stringify(spectra),
+    );
+
+    for (const spectrum of spectra) {
+      const { source } = spectrum;
+      if (source.is1D && !source.isFID) {
+        const { info, meta, spectra } = spectrum;
+        const { observeFrequency, nucleus, data } = spectra[0];
+        const options = {
+          xyEncoding: 'DIFDUP',
+          info: {
+            title: info.TITLE,
+            owner: info.OWNER,
+            origin: info.ORIGIN,
+            dataType: meta.DATATYPE,
+            '.OBSERVE FREQUENCY': observeFrequency,
+            '.OBSERVE NUCLEUS': nucleus[0],
+          },
+          meta: {},
+          isXYData: true,
+          isNMR: true,
+        };
+
+        const variables = {
+          x: {
+            data: xMultiply(data.x, observeFrequency),
+            label: 'Hz',
+            name: 'frequencies',
+            units: 'Hz',
+            symbol: 'X',
+            isDependent: false,
+          },
+          r: {
+            data: data.re,
+            label: 'real',
+            units: 'arbitratry units',
+            name: 'rfrequeeal',
+            symbol: 'R',
+            isDependent: true,
+          },
+          i: {
+            data: data.im,
+            label: 'imaginary',
+            units: 'arbitratry units',
+            name: 'imaginary',
+            symbol: 'I',
+            isDependent: true,
+          },
+        };
+        const jcamp = fromVariables(variables, options);
+        writeFileSync(join(pathToWrite, 'spectraBruker.jdx'), jcamp);
+      }
+    }
+  }
+})();
